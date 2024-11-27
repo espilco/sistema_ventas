d26a326f07f0e9d42d8ee906b66d5f1f82ca09cb
espilco <66812491+espilco@users.noreply.github.com>
2024-11-26 19:43:42 -0500

    Create conexion.php

---
+++ conexion.php
@@ -0,0 +1,16 @@
+<?php
+    $host = "144.217.242.234";
+    $user = "media1@tescanet.com";
+    $clave = "6SDdSNiEfpoxF7V";
+    $bd = "db_sistema_ventas_upn";
+    $port=3306;
+
+    try {
+        $conexion = new PDO("mysql:host=$host;dbname=$bd;charset=utf8", $user, $clave);
+        // Establecer el modo de error de PDO para manejar excepciones
+        $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        echo "No se pudo conectar a la base de datos: " . $e->getMessage() . " " . $host;
+        exit();
+    }
+?>
